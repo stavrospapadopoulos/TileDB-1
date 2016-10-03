b30d60c1f916d3c82ad408f8b17a224b45d4d4a3
spapadop <stavrosp@csail.mit.edu>
2016-10-03 18:21:47 -0400

    Finished preliminary testing and debugging of dense sorted reads

--- core/src/c_api/c_api.cc
+++ core/src/c_api/c_api.cc
@@ -1330,6 +1330,7 @@ int tiledb_array_aio_read(
   aio_request->id_ = (size_t) tiledb_aio_request;
   aio_request->buffers_ = tiledb_aio_request->buffers_;
   aio_request->buffer_sizes_ = tiledb_aio_request->buffer_sizes_;
+  aio_request->mode_ = tiledb_array->array_->mode();
   aio_request->status_ = &(tiledb_aio_request->status_);
   aio_request->subarray_ = tiledb_aio_request->subarray_;
   aio_request->completion_handle_ = tiledb_aio_request->completion_handle_;
@@ -1354,6 +1355,7 @@ int tiledb_array_aio_write(
   aio_request->id_ = (size_t) tiledb_aio_request;
   aio_request->buffers_ = tiledb_aio_request->buffers_;
   aio_request->buffer_sizes_ = tiledb_aio_request->buffer_sizes_;
+  aio_request->mode_ = tiledb_array->array_->mode();
   aio_request->status_ = &(tiledb_aio_request->status_);
   aio_request->subarray_ = tiledb_aio_request->subarray_;
   aio_request->completion_handle_ = tiledb_aio_request->completion_handle_;
